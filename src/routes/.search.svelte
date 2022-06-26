87e547d46df048964b3d4244ffe78a35fd6723a2
Azarattum <azarattum@pm.me>
2022-06-26 14:52:54 +0700

    Added search construction label

--- src/routes/.search.svelte
+++ src/routes/.search.svelte
@@ -3,3 +3,32 @@
 </script>
 
 <Title />
+
+<section>
+  <img
+    src="/images/construction.svg"
+    alt="Under Construction"
+    id="temp-construction"
+  />
+  <label for="temp-construction">We are working on this feature...</label>
+</section>
+
+<style>
+  section {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    text-align: center;
+
+    opacity: 0.7;
+  }
+
+  img {
+    display: block;
+
+    width: 40vw;
+    margin: 1rem;
+  }
+</style>
